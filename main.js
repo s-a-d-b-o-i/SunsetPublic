8f7e6759a1cc1261c42e690ebbe7611de9582387
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2019-10-09 19:59:44 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,86 +1,56 @@
-//import fs and got
-const fs = require('fs');
-const axios = require('axios');
+// Require FS and AXIOS.
+const fs = require('fs'), axios = require('axios');
 
-//create time variables
-var date = new Date();
+// Time Variables
+const date = Math.floor(Date.now() / 1000);
 
-const cityCode = OWM_CITY_CODE;
-const api_key = 'OpenWeatherMap_API_KEY';
-const ifttt_key = 'IFTTT_API_KEY'
-const owm_url = `http://api.openweathermap.org/data/2.5/forecast?id=${cityCode}&APPID=${api_key}`
+// Keys and URLs
+const cityCode = OWM_CITY_CODE, api_key = 'OpenWeatherMap_API_KEY', ifttt_key = 'IFTTT_API_KEY', owm_url = `http://api.openweathermap.org/data/2.5/forecast?id=${cityCode}&APPID=${api_key}`;
 
-async function main(){
-    for(;;){
-        var sunsetLocal = 0;
+setInterval(() => {
+  let sunsetLocal = 0;
+  axios.get(owm_url).then(response => console.log(response.data.city.sunset));
 
-        axios.get('http://api.openweathermap.org/data/2.5/forecast?id=5301388&APPID=f204d06d667a32d5e3224a49fddef6fc')
-            .then((response) => {
-             console.log(response.data.city.sunset);
-        });
+  axios.get(owm_url).then(response => {
+    const sunset = response.data.city.sunset;
+    let sunsetFucc = { time: sunset }, data = JSON.stringify(sunsetFucc);
 
-        axios.get(owm_url).then((response) => {
-            const sunset = response.data.city.sunset;
-            // console.log(sunset);
-            let sunsetFucc = { 
-                time: sunset
-            };
-                
-            let data = JSON.stringify(sunsetFucc);
-            fs.writeFileSync('sunset_cache.json', data);
-        });
-        
-        let rawdata = fs.readFileSync('sunset_cache.json');
-        let sunsetJSON = JSON.parse(rawdata);
-        sunsetLocal = sunsetJSON.time;
-        var timestampLocal = Math.floor(date.getTime() / 1000);
-        var timeto = sunsetLocal-timestampLocal;
+    fs.writeFileSync('sunset_cache.json', data);
+  });
+
+  let rawdata = fs.readFileSync('sunset_cache.json'), sunsetJSON = JSON.parse(rawdata);
+  sunsetLocal = sunsetJSON.time;
+  let timeto = sunsetLocal - date;
 
 
-        console.log(timestampLocal);
-        console.log(sunsetLocal)
-        console.log(`${timeto} seconds left`);
+  console.log(date);
+  console.log(sunsetLocal);
+  console.log(`${timeto} seconds left`);
 
-        //HOURLY CHECK
-        axios.post(`https://maker.ifttt.com/trigger/test_check/with/key/${ifttt_key}`, {
-            value1: `${sunsetLocal}`,
-            value2: Math.floor(timeto/60) + " minutes left."
-        });
+  // Hourly Check
+  axios.post(`https://maker.ifttt.com/trigger/test_check/with/key/${ifttt_key}`, {
+    value1: `${sunsetLocal}`,
+    value2: `${Math.floor(timeto / 60)} minutes left.`,
+  });
 
-        for(i=0; i<60; i++){
-            //create local variable for storing the sunset time
-            var sunsetLocal = 0;
-            var timestamp = Math.floor(date.getTime() / 1000);
+  for (let i = 0; i < 60; i++) {
+    // Create local variable for storing the sunset time
+    sunsetLocal = 0;
 
-            //read the current JSON information
-            let rawdata = fs.readFileSync('sunset_cache.json');
-            let sunsetJSON = JSON.parse(rawdata);
-            sunsetLocal = sunsetJSON.time;
+    // Read the current JSON information
+    rawdata = fs.readFileSync('sunset_cache.json');
+    sunsetJSON = JSON.parse(rawdata);
+    sunsetLocal = sunsetJSON.time;
+    console.log(sunsetLocal);
 
-            console.log(sunsetLocal)
-
-            //Execute sunset check
-            if(timestamp>=sunsetLocal && timestamp<=(sunsetLocal+300000)){
-                //send post request to IFTTT
-                axios.post(`https://maker.ifttt.com/trigger/light_on_sunset/with/key/${ifttt_key}`);
-                console.log('sunset time');
-            }else{
-                console.log('no sunset L')
-            }
-            // console.log('yeetus');
-            // console.log(i);
-            let delayres = await delay(60000);
-            
-        }
+    // Execute sunset check
+    if (date >= sunsetLocal && date <= (sunsetLocal + 300000)) {
+      // Send post request to IFTTT
+      axios.post(`https://maker.ifttt.com/trigger/light_on_sunset/with/key/${ifttt_key}`);
+      console.log('Sunset');
+    } else {
+      console.log('No Sunset');
     }
-}
+  }
+}, 60000);
 
-async function delay(delayInms) {
-    return new Promise(resolve  => {
-      setTimeout(() => {
-        resolve(2);
-      }, delayInms);
-    });
-}
-
-main();
